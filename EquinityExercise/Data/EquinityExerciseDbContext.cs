9a15e67d68253b6c5bb48bf3305ca0bea9ea2823
papryk <patryknng@interia.pl>
2019-09-08 22:00:06 +0200

    Add project files.

---
+++ EquinityExercise/Data/EquinityExerciseDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EquinityExercise.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EquinityExercise.Data
+{
+    public class EquinityExerciseDbContext : DbContext
+    {
+        public DbSet<HiringManager> HiringManager { get; set; }
+        public DbSet<Interview> Interviews { get; set; }
+        public DbSet<JobCandidate> JobCandidates { get; set; }
+
+        public EquinityExerciseDbContext()
+        {
+
+        }
+
+
+    }
+}
---
+++ EquinityExercise/Models/JobCandidate.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace EquinityExercise.Models
+{
+    public class JobCandidate
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
